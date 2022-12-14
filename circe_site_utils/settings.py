518b5cccf35956d8bbde2f5141f52fd642faa751
fabiofucci <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>
2022-12-14 23:18:31 +0100

    Deploy su Kubernetes DigitalOcean

--- circe_site_utils/settings.py
+++ circe_site_utils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w8*u*0&&je=ca7+k!@pzuhwwlqr25f3jc#ug3$b7&oox8cf!hk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.circe.cloud']
 
 
 # Application definition
