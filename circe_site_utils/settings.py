718376261d22721bd7ca8d3cf7617d71f9dc86be
fabiofucci <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>
2022-12-15 17:03:20 +0100

    CORS per ricevere richieste da Poseidone

--- circe_site_utils/settings.py
+++ circe_site_utils/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+"https://www.circe.cloud",
+"https://poseidone.circe.cloud"
+]
 
 # INVIO EMAIL
 EMAIL_HOST = os.environ.get('SMTPS_HOST')
